7826679a3745feeb7f19c767b851c73e43c69bf5
Niklas <114483978+CloudlyBot@users.noreply.github.com>
2024-11-01 17:30:50 +0000

    Updateded for 1.21

--- src/main/java/de/butzlabben/world/util/VersionUtil.java
+++ src/main/java/de/butzlabben/world/util/VersionUtil.java
@@ -15,11 +15,12 @@ public class VersionUtil {
     private VersionUtil() {
     }
 
-    /*public static int getVersion() {
+    public static int getVersion() {
         if (version == 0) {
             // Detect version
             String v = Bukkit.getVersion();
-            if (v.contains("1.20")) version = 20;
+            if (v.contains("1.21")) version = 21;
+            else if (v.contains("1.20")) version = 20;
             else if (v.contains("1.19")) version = 19;
             else if (v.contains("1.18")) version = 18;
             else if (v.contains("1.17")) version = 17;
@@ -44,33 +45,7 @@ public class VersionUtil {
             version = 12;
         }
         return version;
-    }*/
-
-    public static int getVersion() {
-        if (version == 0) {
-            // Detect version
-            String v = Bukkit.getVersion();
-            
-            // Extract minor version number
-            int minorVersion = 0;
-            if (v.matches(".*1\\.(\\d+).*")) {
-                minorVersion = Integer.parseInt(v.replaceAll(".*1\\.(\\d+).*", "$1"));
-            }
-    
-            // Set version based on minor version number
-            switch (minorVersion) {
-                case 21 -> version = 21;
-                case 20 -> version = 20;
-                case 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3 -> version = minorVersion;
-                default -> {
-                    System.err.println("[WorldSystem] Unknown version: " + v);
-                    System.err.println("[WorldSystem] Choosing version 1.12.2");
-                    version = 12; // Default to 1.12 if version is unrecognized
-                }
-            }
-        }
-        return version;
-    }    
+    }
 
     public static boolean isCancelled(BukkitTask task) {
         if (getVersion() <= 12)
